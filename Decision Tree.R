3410032473bac9efeb937499a6b40797e953a154
FanxiuB <bfanxiu@gmail.com>
2023-03-23 22:09:48 +0000

    Classification tree code finished

--- Decision Tree.R
+++ Decision Tree.R
@@ -27,6 +27,7 @@ for(i in c(2:10,15,21)){
 }
 
 data_tree <- data[,-c(5,10,13,16,20)]
+describe(data_tree)
 
 set.seed(2023)
 split <- partition(data_tree$age, p = c(train = 0.6, valid = 0.2, test = 0.2))
@@ -34,8 +35,8 @@ train <- data_tree[split$train, ]
 valid <- data_tree[split$valid, ]
 test <- data_tree[split$test, ]
 
-##Modelling Decision Tree
-#Model1
+##Modelling Classification Tree
+#Classification Tree
 set.seed(2023)
 
 minsplit=100
@@ -44,10 +45,10 @@ negativeWeight = 1.0 / (nrow(subset(train, y!= "yes")) / nrow(train))
 modelWeights <- ifelse(train$y== "yes", positiveWeight, negativeWeight)
 
 train.Model <- rpart(y ~., data=train, method="class", weights=modelWeights,
-                     parms=list(split='information'),
                      control=rpart.control(minsplit=100, minbucket=round(minsplit/3),
-                                           maxdepth = 4, cp=0.02))        
+                                           maxdepth = 4, cp=0.015))        
 rpart.plot(train.Model, type=2, extra=4)
+
 #Variable Importance
 train.Model$variable.importance
 
@@ -61,60 +62,40 @@ table(valid[,16], valid.pred)
 
 #Evaluation
 train.table <- table(train[,16], train.pred)
-train.table[1,1]/sum(train.table[1,]) # training sensitivity
-train.table[2,2]/sum(train.table[2,]) # training specificity
-(train.table[1,1]+train.table[2,2])/sum(train.table) # Total accuracy
+train.table[1,1]/sum(train.table[1,]) #training sensitivity
+train.table[2,2]/sum(train.table[2,]) #training specificity
+(train.table[1,1]+train.table[2,2])/sum(train.table) #training accuracy
 
 valid.table <- table(valid[,16], valid.pred)
-valid.table[1,1]/sum(valid.table[1,]) # training sensitivity
-valid.table[2,2]/sum(valid.table[2,]) # training specificity
-(valid.table[1,1]+valid.table[2,2])/sum(valid.table) # Total accuracy
+valid.table[1,1]/sum(valid.table[1,]) #validation sensitivity
+valid.table[2,2]/sum(valid.table[2,]) #validation specificity
+(valid.table[1,1]+valid.table[2,2])/sum(valid.table) #validation accuracy
 
 Ynew.pred <- predict(train.Model, newdata=test, type="class")
 test.table <- table(test[,16], Ynew.pred)
-test.table[1,1]/sum(test.table[1,]) # training sensitivity
-test.table[2,2]/sum(test.table[2,]) # training specificity
-(test.table[1,1]+test.table[2,2])/sum(test.table) # Total accuracy
-
-
-
-
-
-set.seed(2023)
-single_tree <- rpart(y~., data=train)
-predictions <- predict(single_tree, test, "class")
-overall_class_rate_tree <- mean(predictions==test$y)
-print(c(overall_class_rate_bag,overall_class_rate_tree))
-
-
-bag_df <- data.frame(var = rownames(randomForest::importance(bagging)),
-                     MeanDecreaseGini = randomForest::importance(bagging)[, 1]) %>%
-  mutate(var = fct_reorder(var, MeanDecreaseGini, median))
-bag_ggplot <- ggplot(bag_df, aes(var, MeanDecreaseGini)) +
-  geom_point() +
-  coord_flip() +
-  labs(title = "Predicting credit approval on the German Credit Data",
-       subtitle = "Bagging", x = NULL, y = "Average decrease in the Gini Index") +
-  theme(plot.title = element_text(hjust = 0.5, size=10),
-        plot.subtitle = element_text(hjust = 0.5))
-bag_ggplot
+test.table[1,1]/sum(test.table[1,]) #sensitivity
+test.table[2,2]/sum(test.table[2,]) #specificity
+(test.table[1,1]+test.table[2,2])/sum(test.table) #accuracy
 
 #Random Forest
-n<-length(names(train))     #计算数据集中自变量个数，等同n=ncol(train_data)
-rate=1     #设置模型误判率向量初始值
+#Select mtry
+n<-length(names(train))     
+rate=1     
 for(i in 1:(n-1)){
   set.seed(1234)
   rf_train<-randomForest(y~.,data=train, mtry=i, ntree=1000)
-  rate[i]<-mean(rf_train$err.rate)   #计算基于OOB数据的模型误判率均值
+  rate[i]<-mean(rf_train$err.rate)   
   print(rf_train)    
 }
-rate     #展示所有模型误判率的均值
+rate     
 plot(rate)
 
+#Select ntree
 set.seed(2023)
 rf_train<-randomForest(y~.,data=train, mtry=5, ntree=1000)
-plot(rf_train)    #绘制模型误差与决策树数量关系图  
+plot(rf_train)   
 
+#Random Forest model building
 set.seed(2023)
 rf <- randomForest(y~., data=train, mtry=5, ntree=200, importance=TRUE, weights=modelWeights)
 rf
@@ -125,20 +106,14 @@ table(valid[,16], valid.pred)
 
 #Evaluation
 train.table <- rf$confusion[,-3]
-train.table[1,1]/sum(train.table[1,]) # training sensitivity
-train.table[2,2]/sum(train.table[2,]) # training specificity
+train.table[1,1]/sum(train.table[1,]) #training sensitivity
+train.table[2,2]/sum(train.table[2,]) #training specificity
 (train.table[1,1]+train.table[2,2])/sum(train.table) # Total accuracy
 
 valid.table <- table(valid[,16], valid.pred)
-valid.table[1,1]/sum(valid.table[1,]) # training sensitivity
-valid.table[2,2]/sum(valid.table[2,]) # training specificity
-(valid.table[1,1]+valid.table[2,2])/sum(valid.table) # Total accuracy
-
-Ynew.pred <- predict(rf, newdata=test, type="class")
-test.table <- table(test[,16], Ynew.pred)
-test.table[1,1]/sum(test.table[1,]) # training sensitivity
-test.table[2,2]/sum(test.table[2,]) # training specificity
-(test.table[1,1]+test.table[2,2])/sum(test.table) # Total accuracy
+valid.table[1,1]/sum(valid.table[1,]) #validation sensitivity
+valid.table[2,2]/sum(valid.table[2,]) #validation specificity
+(valid.table[1,1]+valid.table[2,2])/sum(valid.table) #Validation accuracy
 
 randomForest::importance(rf)[, 1]
 rf_df <- data_frame(var = rownames(randomForest::importance(rf)),
@@ -147,10 +122,14 @@ rf_df <- data_frame(var = rownames(randomForest::importance(rf)),
 rf_ggplot <- ggplot(rf_df, aes(var, MeanDecreaseGini)) +
   geom_point() +
   coord_flip() +
-  labs(title = "Predicting credit approval on the German Credit Data",
-       subtitle = "randomForest", x = NULL, y = "Average decrease in the Gini Index") +
+  labs(title = "Predicting subscription in Bank Marketing data",
+       x = NULL, y = "Mean Decrease Gini") +
   theme(plot.title = element_text(hjust = 0.5, size=10),
         plot.subtitle = element_text(hjust = 0.5))
 rf_ggplot
 
-
+Ynew.pred <- predict(rf, newdata=test, type="class")
+test.table <- table(test[,16], Ynew.pred)
+test.table[1,1]/sum(test.table[1,]) #sensitivity
+test.table[2,2]/sum(test.table[2,]) #specificity
+(test.table[1,1]+test.table[2,2])/sum(test.table) #accuracy
